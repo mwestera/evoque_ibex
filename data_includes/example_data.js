05d2bf7707bb0d1b8d0435cf8c20ff8d626920a7
mwestera <matthijs.westera@gmail.com>
2021-03-24 12:06:27 +0100

    record urlparams

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -3,6 +3,19 @@ var shuffleSequence = seq("consent", "introform", "training", "warning", sepWith
 //var shuffleSequence = seq("setcounter", sepWith("sep", "fragment"));    // Without intro, with counter
 var practiceItemTypes = ["practice"];
 
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+
 var globalBuffer = "INIT";
 
 var numQuestionsDone = 0;
